a491153d8fbc230aa0fe0caedecb1ace16ec40dc
Fabian <fabian.jaser@gmail.com>
2021-02-10 14:48:37 +0100

    〽️ add first implementations

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-const Discord = require("discord.js");
+"use strict";
+
+const { Client, MessageEmbed } = require("discord.js");
 const config = require("./config.json");
 
-const client = new Discord.Client();
+const client = new Client();
 
-const prefix = "#"
+const prefix = "#";
 
-client.on("message", function(message){
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
+client.on("ready", () => {
+  console.log("I am all set up!");
+});
 
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(' ');
-    const command = args.shift().toLowerCase();
+client.on("message", function (message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
-    if (command === "ping") {
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(`pong. I hate ts. ${timeTaken}`)
-    }
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(" ");
+  const command = args.shift().toLowerCase();
+
+  if (command === "replytome") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`I hate ts. ${timeTaken}`);
+    console.log(message.channel.id);
+  }
+
+  if (command === "embed") {
+    const embed = new MessageEmbed()
+      .setTitle("just testing embed, this is the title")
+      .setColor(0xff0000)
+      .setDescription("this is the description");
+    message.channel.send(embed);
+  }
 });
 
 client.login(config.BOT_TOKEN);
